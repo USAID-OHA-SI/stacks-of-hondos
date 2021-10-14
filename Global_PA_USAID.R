8e00f84107550f2a0bb1b3df1bb4433bda3901ca
Jairo Montes <jmontespenaloza@usaid.gov>
2021-10-14 12:05:26 -0400

    New updates

---
+++ Global_PA_USAID.R
@@ -0,0 +1,51 @@
+
+
+
+get_global_pa_usaid<-function(df,funding_agency="fundingagency", ou="operatingunit"){
+  df<-df%>%
+    prep_fsd()%>%
+    
+    #filter for fiscal year
+    dplyr::filter(fiscal_year=="2020" | fiscal_year=="2021")%>%
+    #dplyr::filter(fundingagency == "USAID") %>% 
+    
+    #filter for OU
+    #dplyr::filter(operatingunit== "Mozambique")%>%
+    dplyr::filter(operatingunit %in% ou)%>%
+    dplyr::filter(fundingagency %in% funding_agency) %>% 
+    
+    #select specific variables
+    dplyr::select (c(fundingagency,program, fiscal_year,cop_budget_total,expenditure_amt))%>%
+    mutate_at(vars(cop_budget_total,expenditure_amt),~replace_na(.,0))%>%
+    #mutate( fundingagency = fct_relevel(fundingagency, "USAID","CDC"))%>%
+    
+    
+    
+    group_by(program,fiscal_year)%>%
+    summarise_at(vars(cop_budget_total,expenditure_amt), sum, na.rm = TRUE)%>%
+    dplyr::mutate(budget_execution=percent_clean(expenditure_amt,cop_budget_total))%>%
+    ungroup()%>%
+    
+    
+    pivot_wider(names_from = fiscal_year,values_from = cop_budget_total:budget_execution, values_fill = 0)%>%
+    dplyr::relocate(expenditure_amt_2020, .before = cop_budget_total_2020) %>%
+    dplyr::relocate(expenditure_amt_2021, .before = cop_budget_total_2021) %>%
+    dplyr::relocate(budget_execution_2021, .after = cop_budget_total_2021)%>%
+    dplyr::relocate(budget_execution_2020, .after = cop_budget_total_2020) %>%
+    
+    #break into separate functions
+    
+    ea_style()%>%
+    #gt()%>%
+    cols_label(
+      program = "Program Area")%>%
+    tab_header(
+      title = glue::glue("{funding_agency} COP2019 & COP2020 {ou} Financial Performance Summary"),
+      subtitle = legend_chunk) %>%
+  
+  tab_options(footnotes.font.size = "small")
+  
+  return(df)
+}
+
+get_global_pa_usaid(df_fsd,"CDC", "Malawi")
