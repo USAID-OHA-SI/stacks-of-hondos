c5a6620b06aca4cf10837f0dffad6ec93a2fe875
Jairo Montes <jmontespenaloza@usaid.gov>
2021-11-10 11:13:50 -0500

    Update prep_fsd.R

--- prep_fsd.R
+++ prep_fsd.R
@@ -1,11 +1,4 @@
-# PURPOSE: Creation of base table
-# AUTHOR: EA team | SIEI
-# LICENSE: MIT
-# DATE: 2021-09-30
-# NOTES: use this function to create the main table that you can then pass through different gt helpers
-#functions
 
-# LOCALS & SETUP ============================================================================
 
 #use this function to read to clean and read in financial dataset
 prep_fsd <-function(df){ 
@@ -15,9 +8,9 @@ prep_fsd <-function(df){
     dplyr::select(-c("prime_partner_duns","prime_partner_org_type",
                                   "is_indigenous_prime_partner", "subrecipient_duns",
                                   "award_number","procurement_type")) %>% 
-    glamr::remove_mo()%>% # filter out M&O
-    #dplyr::rename("fiscal_year"= implementation_year) %>% 
-    #dplyr::filter(fiscal_year == c(2020 ,2021))
+    # filter out M&O
+    glamr::remove_mo()%>% 
+    
   
  
   ##concatenate mech id and mech name
@@ -25,12 +18,10 @@ prep_fsd <-function(df){
   
   #mutate data type double into integer to have round numbers
   dplyr::mutate_if(is.double, as.integer)%>%
-  #mutate data type for mechanism id and fiscal year
-  #df<-df %>% dplyr::mutate_at(vars(mech_code, fiscal_year),list(as.character))
+ 
   
   #drop NA for numeric amounts
-  
-    mutate_at(vars(cop_budget_new_funding:expenditure_amt),~replace_na(.,0))%>%
+  mutate_at(vars(cop_budget_new_funding:expenditure_amt),~replace_na(.,0))%>%
   
   
   #recode values to match naming in Financial Integrated Dataset
