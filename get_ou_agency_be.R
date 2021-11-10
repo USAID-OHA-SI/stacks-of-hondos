e20a13f9ce2328c4e5026725876c5197040129b6
Benjamin Kasdan <62769562+benjaminnk@users.noreply.github.com>
2021-11-10 17:31:02 -0500

    added in google drive output

--- get_ou_agency_be.R
+++ get_ou_agency_be.R
@@ -38,7 +38,6 @@ get_ou_agency_be<-function(df, ou="operatingunit"){
     dplyr::relocate(expenditure_amt_2021, .before = cop_budget_total_2021) %>%
     dplyr::relocate(budget_execution_2021, .after = cop_budget_total_2021)%>%
     dplyr::relocate(budget_execution_2020, .after = cop_budget_total_2020) %>%
-   
     ea_style()%>%
     cols_label(
      fundingagency = "Funding Agency")%>%
@@ -55,11 +54,17 @@ table_out<-"GitHub/stacks-of-hondos/Images/budget execution"
 #to run for one OU below. Be sure to name the ou 
 get_ou_agency_be(df_fsd, "South Africa")%>%
   gtsave(.,path=table_out,"test_be.png")
-#to run for all ous. You can also use country_list
+#to run for all ous
 purrr::map(ou_list, ~get_ou_agency_be(df_fsd, ou = .x)%>%
              gtsave(.,path=table_out,filename = glue::glue("{.x}_budget_execution.png")))
 
+#Uploading to google drive===============================================
+source("~/GitHub/stacks-of-hondos/upload_dir_to_gdrive.R")
 
+local_p <- table_out
+g_path <- '1V_58kCkggfpY89_-C1rmmrIn4wHzGJ_D'
+
+upload_dir_to_gdrive(local_p, g_path)
 
 
 
