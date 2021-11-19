b384dd02c9f5f7dc9ecd48e16ad9f89f22a5b73e
Benjamin Kasdan <62769562+benjaminnk@users.noreply.github.com>
2021-11-19 15:25:24 -0500

    Update get_ou_agency_be.R

--- get_ou_agency_be.R
+++ get_ou_agency_be.R
@@ -59,7 +59,7 @@ purrr::map(ou_list, ~get_ou_agency_be(df_fsd, ou = .x)%>%
              gtsave(.,path=table_out,filename = glue::glue("{.x}_budget_execution.png")))
 
 #Uploading to google drive===============================================
-source("~/GitHub/stacks-of-hondos/upload_dir_to_gdrive.R")
+source("~/GitHub/EA-Utilities/upload_dir_to_gdrive.R")
 
 local_p <- table_out
 g_path <- '1V_58kCkggfpY89_-C1rmmrIn4wHzGJ_D'
