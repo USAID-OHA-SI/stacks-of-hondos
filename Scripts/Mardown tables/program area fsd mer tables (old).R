bd643f20cbde00bc418eb76b9816d48bd089f9ff
Benjamin Kasdan <62769562+benjaminnk@users.noreply.github.com>
2021-12-29 14:40:56 -0500

    updates

--- Scripts/Mardown tables/program area fsd mer tables (old).R
+++ Scripts/Mardown tables/program area fsd mer tables (old).R
@@ -21,9 +21,8 @@ df_msd<-si_path()%>%
 #Be sure to load the following source files below before running
 source("~/GitHub/stacks-of-hondos/Scripts/utilities.R")
 
-indics<-c("OVC_SERV")
-progs<-c("OVC")
-
+indics<-c("HTS_TST","HTS_TST_POS", "TX_CURR", "TX_NEW","OVC_SERV")
+progs<-c("HTS", "C&T","OVC")
 
 
 
@@ -37,10 +36,10 @@ df_fsd<-df_fsd%>%
                                            
                                            
                                            TRUE ~program))%>%
-  group_by(operatingunit,fundingagency,fiscal_year, mech_code, mech_name, primepartner, program,) %>%
-  
+  group_by(operatingunit,fundingagency,fiscal_year, mech_code, mech_name, primepartner, program) %>% 
   #group_by(country, mech_code, mech_name, primepartner, fiscal_year, `Program Area: Sub Program Area-Service Level`,`Beneficiary-Sub Beneficiary`)%>%
   summarise_at(vars(cop_budget_total, expenditure_amt), sum, na.rm = TRUE) %>% 
+  filter(fiscal_year="2021")%>%
   ungroup()%>%
   filter(program %in% progs)%>%
   mutate(budget_execution=round(expenditure_amt/cop_budget_total*100))
@@ -59,8 +58,11 @@ df_msd<-df_msd%>%
   summarise_at(vars(cumulative,targets), sum, na.rm = TRUE) %>% 
   ungroup()%>%
   
-  dplyr::mutate(program = dplyr::case_when(indicator    == "OVC_SERV"    ~"OVC", 
-                                           
+  dplyr::mutate(program = dplyr::case_when(indicator    == "TX_CURR"    ~"C&T", 
+                                           indicator    == "TX_NEW"    ~"C&T",
+                                           indicator =="HTS_TST" ~"HTS",
+                                           indicator == "HTS_TST_POS" ~"HTS",
+                                           indicator    == "OVC_SERV"    ~"OVC",
                                            
                                            TRUE ~indicator))
 df_msd<-df_msd%>%
@@ -79,17 +81,53 @@ df_msd<-df_msd%>%
 
 
 #join datasets together 
-df_ue<-full_join(df_fsd,df_msd)%>%
+df_ue<-left_join(df_fsd,df_msd)%>%
+  filter(fiscal_year=="2021")%>%
   mutate_at(vars(cop_budget_total: OVC_SERV),~replace_na(.,0))%>%
   mutate(mech=glue("{mech_code}-{mech_name}"))%>%
   relocate(expenditure_amt, .before= cop_budget_total)%>%
   relocate(mech, .before= expenditure_amt)%>%
-  filter(fiscal_year=="2021")%>%
-  filter(operatingunit %in% ou)%>%
-  filter(OVC_SERV>0
+ 
+  select(operatingunit,fundingagency,program,mech:OVC_SERV)
+  
+get_program_specfic<-function(df, ou="operatingunit",programs=c("HTS","C&T","OVC")){
+  
+  df<-df_ue%>%
+    filter(program %in% programs,
+           operatingunit %in% ou)%>%
+    select_if(~!( all(. == 0)))%>%
+    select(-(operatingunit))
+  return(df)
+}
+
+
+df_hts<-df_ue%>%
+  filter(program=="HTS",
+         
+         operatingunit %in% ou)%>%
+  select_if(~!( all(. == 0)))%>%
+   filter(HTS_TST>0 | HTS_TST_POS>0
+          |budget_execution>0)%>%
+  select(-(operatingunit))
+
+df_ct<-df_ue%>%
+  filter(program=="C&T",
+         
+         operatingunit %in% ou)%>%
+  
+select_if(~!( all(. == 0)))%>%
+  select(operatingunit:budget_execution,TX_CURR,TX_NEW)%>%
+  filter(TX_CURR>0 | TX_NEW>0
+         |budget_execution>0)%>%
+  select(fundingagency,mech:TX_NEW)
+
+df_ovc<-df_ue%>%
+  filter(program=="OVC",
+        
+         operatingunit %in% ou)%>%
+  select_if(~!( all(. == 0)))%>%
+  select(operatingunit:budget_execution,OVC_SERV)%>%
+  filter(OVC_SERV>0 
          |budget_execution>0)%>%
   select(fundingagency,mech:OVC_SERV)
-
-  
  
-
--- Scripts/Mardown tables/program area fsd mer tables.R
+++ Scripts/Mardown tables/program area fsd mer tables.R
@@ -19,8 +19,6 @@ df_msd<-si_path()%>%
 #This function can be used to generate unit expenditure tables across the treatment continuum
 #It can be used to generate one table for one ou, or a batch for all ous.
 #Be sure to load the following source files below before running
-source("~/GitHub/stacks-of-hondos/Scripts/utilities.R")
-
 indics<-c("HTS_TST","HTS_TST_POS", "TX_CURR", "TX_NEW","OVC_SERV")
 progs<-c("HTS", "C&T","OVC")
 
@@ -39,6 +37,7 @@ df_fsd<-df_fsd%>%
   group_by(operatingunit,fundingagency,fiscal_year, mech_code, mech_name, primepartner, program) %>% 
   #group_by(country, mech_code, mech_name, primepartner, fiscal_year, `Program Area: Sub Program Area-Service Level`,`Beneficiary-Sub Beneficiary`)%>%
   summarise_at(vars(cop_budget_total, expenditure_amt), sum, na.rm = TRUE) %>% 
+  
   ungroup()%>%
   filter(program %in% progs)%>%
   mutate(budget_execution=round(expenditure_amt/cop_budget_total*100))
@@ -81,39 +80,21 @@ df_msd<-df_msd%>%
 
 #join datasets together 
 df_ue<-left_join(df_fsd,df_msd)%>%
+  filter(fiscal_year=="2021")%>%
   mutate_at(vars(cop_budget_total: OVC_SERV),~replace_na(.,0))%>%
   mutate(mech=glue("{mech_code}-{mech_name}"))%>%
   relocate(expenditure_amt, .before= cop_budget_total)%>%
-  relocate(mech, .before= expenditure_amt)
+  relocate(mech, .before= expenditure_amt)%>%
+  select(operatingunit,fundingagency,program,mech:OVC_SERV)
+
+#Use below function to get HTS, C&T, OVC info for a specific OU
   
- 
-
-df_hts<-df_ue%>%
-  filter(program=="HTS",
-         fiscal_year=="2021",
-         operatingunit %in% ou)%>%
-  select_if(~!( all(. == 0)))%>%
-   filter(HTS_TST>0 | HTS_TST_POS>0
-          |budget_execution>0)%>%
-  select(fundingagency,mech:HTS_TST_POS)
-
-df_ct<-df_ue%>%
-  filter(program=="C&T",
-         fiscal_year=="2021",
-         operatingunit %in% ou)%>%
-  select_if(~!( all(. == 0)))%>%
-  select(operatingunit:budget_execution,TX_CURR,TX_NEW)%>%
-  filter(TX_CURR>0 | TX_NEW>0
-         |budget_execution>0)%>%
-  select(fundingagency,mech:TX_NEW)
-
-df_ovc<-df_ue%>%
-  filter(program=="OVC",
-         fiscal_year=="2021",
-         operatingunit %in% ou)%>%
-  select_if(~!( all(. == 0)))%>%
-  select(operatingunit:budget_execution,OVC_SERV)%>%
-  filter(OVC_SERV>0 
-         |budget_execution>0)%>%
-  select(fundingagency,mech:OVC_SERV)
- 
+get_program_specfic<-function(df, ou="operatingunit",programs=c("HTS","C&T","OVC")){
+  
+  df<-df_ue%>%
+    filter(program %in% programs,
+           operatingunit %in% ou)%>%
+    select_if(~!( all(. == 0)))%>%
+    select(-(operatingunit))
+  return(df)
+}
