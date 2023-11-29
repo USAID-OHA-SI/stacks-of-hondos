81afe4db9d5c0bfb7883c92d21a5928925067485
Jairo Montes <jmontespenaloza@usaid.gov>
2023-11-29 10:18:31 -0500

    Update get_global_be.R

--- Scripts/get_global_be.R
+++ Scripts/get_global_be.R
@@ -18,7 +18,7 @@ source("~/GitHub/stacks-of-hondos/Scripts/utilities.R")
 
 ### Function ===================================================================
 get_global_agency_be<-function(df){
-  df<-df%>%
+  df<-df_fsd%>%
     prep_fsd()%>%
     dplyr::filter(fiscal_year %in% fys)%>%
     filter(!operatingunit=="Ukraine")%>%
@@ -28,10 +28,10 @@ get_global_agency_be<-function(df){
     dplyr::mutate(budget_execution=percent_clean(expenditure_amt,cop_budget_total))%>%
     ungroup()%>%
     pivot_wider(names_from = fiscal_year,values_from = cop_budget_total:budget_execution, values_fill = 0)%>%
+    dplyr::relocate(expenditure_amt_2023, .before = cop_budget_total_2023) %>%
     dplyr::relocate(expenditure_amt_2022, .before = cop_budget_total_2022) %>%
-    dplyr::relocate(expenditure_amt_2021, .before = cop_budget_total_2021) %>%
-    dplyr::relocate(budget_execution_2021, .after = cop_budget_total_2021)%>%
-    dplyr::relocate(budget_execution_2022, .after = cop_budget_total_2022) %>%
+    dplyr::relocate(budget_execution_2022, .after = cop_budget_total_2022)%>%
+    dplyr::relocate(budget_execution_2023, .after = cop_budget_total_2023) %>%
     #gt()%>%
     ea_style()%>%
     cols_label( #update GT to check on tidy select), also look at clean_names, also potentially case_when
@@ -52,12 +52,12 @@ get_global_agency_be<-function(df){
 # Read in the FSD dataframe from where it is locally saved
 df_fsd<-si_path()%>%
   return_latest("Fin")%>%
-  read_msd()
+  read_psd()
 
 # Output: 
 #       * Feel free to change the path you are saving the file to, by changing
 #         the variable "table_out"
-table_out<-"GitHub/stacks-of-hondos/Images/Global Performance"
+table_out<-"GitHub/stacks-of-hondos/Images"
 get_global_agency_be(df_fsd)%>%
   gtsave(., path=table_out, filename="global performance_all_agencies.png")
 
